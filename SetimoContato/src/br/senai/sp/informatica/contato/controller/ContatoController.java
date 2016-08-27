f25095df3c46104a00c4330242d943451f821a72
Chile <jrcs.2008@yahoo.com.br>
2016-08-27 12:32:53 -0300

    Commit 5

--- SetimoContato/src/br/senai/sp/informatica/contato/controller/ContatoController.java
+++ SetimoContato/src/br/senai/sp/informatica/contato/controller/ContatoController.java
@@ -1,14 +1,19 @@
 package br.senai.sp.informatica.contato.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.senai.sp.informatica.contato.dao.ContatoDao;
 import br.senai.sp.informatica.contato.modelo.Contato;
 import br.senai.sp.informatica.contato.modelo.Sexo;
 
 @Controller
 public class ContatoController {
+	@Autowired
+	private ContatoDao dao;
 
 	@RequestMapping("/formContato")
 	public String form(Model model) {
@@ -16,9 +21,10 @@ public class ContatoController {
 		return "/contato/formulario";
 	}
 
+	@Transactional
 	@RequestMapping("/salvarContato")
 	public String salvar(Contato contato) {
-		System.out.println(contato.getNome());
+		dao.salvar(contato);
 		return "/contato/sucesso";
 	}
 }
--- SetimoContato/src/br/senai/sp/informatica/contato/dao/ContatoDao.java
+++ SetimoContato/src/br/senai/sp/informatica/contato/dao/ContatoDao.java
@@ -1,10 +1,15 @@
 package br.senai.sp.informatica.contato.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
 
 import br.senai.sp.informatica.contato.modelo.Contato;
 
+@Repository
 public class ContatoDao {
+	@PersistenceContext
 	private EntityManager manager;
 
 	public void salvar(Contato contato) {
